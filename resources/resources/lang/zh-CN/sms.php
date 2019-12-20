792b3c15de0e8ff3e1d442e6021a220c023e1e54
pianqifei <1726441799@qq.com>
2019-12-20 11:49:33 +0800

    删除多余资源

--- resources/resources/lang/zh-CN/sms.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'sms_err'=>'验证码错误或过期',
-    'sms_temp'=>'您的验证码是:code。它的有效期10分钟，不要与他人分享您的验证码。',
-    'sms_limit'=>'短信发送次数过多',
-    'send_success'=>'发送成功',
-    'send_failed'=>'发送失败',
-];
